b965e64120944090b8f8c20283e4fd1f34efd39d
Andra526 <andrasetya856@gmail.com>
2026-06-26 22:17:33 +0700

    Fix: menggunakan direct hardcode keys untuk supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Tambahkan https:// di awal URL
+const supabaseUrl = 'https://fumcrznhvregdsfehiso.supabase.co';
+const supabaseAnonKey = 'sb_publishable_5WmsaED8eeGPbLtVQsXQJQ_lRpU-_4b';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
