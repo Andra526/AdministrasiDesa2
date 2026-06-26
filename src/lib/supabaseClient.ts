46685e8f62cc6a542f66f3d50056290e09d15709
Andra526 <andrasetya856@gmail.com>
2026-06-26 21:59:42 +0700

    lastupdate

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Tambahkan https:// di awal URL
-const supabaseUrl = 'https://fumcrznhvregdsfehiso.supabase.co';
-const supabaseAnonKey = 'sb_publishable_5WmsaED8eeGPbLtVQsXQJQ_lRpU-_4b';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
